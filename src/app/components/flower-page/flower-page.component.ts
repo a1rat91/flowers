043c61c3ab86f3f9e9493785bf6d782839e7b15c
a1rat91 <a1rat91@yandex.ru>
2019-08-21 23:00:49 +0300

    test

--- src/app/components/flower-page/flower-page.component.ts
+++ src/app/components/flower-page/flower-page.component.ts
@@ -1,50 +1,43 @@
-import {AfterViewInit, Component, ElementRef, EventEmitter, OnInit, Output, ViewChild} from '@angular/core';
+import {
+    AfterViewInit, Component, ElementRef, EventEmitter, OnInit, Output, ViewChild,
+    ViewEncapsulation
+} from '@angular/core';
 import {ActivatedRoute, Params} from '@angular/router';
 import {Page, FlowersService} from '../../services/flowers.service';
 import {DistortionEffect} from '../../plugins/distortion-effect';
+
 // import {imagesLoaded} from 'imagesLoaded';
 
 @Component({
-  selector: 'app-flower-page',
-  templateUrl: './flower-page.component.html',
-  styleUrls: ['./flower-page.component.scss']
+    selector: 'app-flower-page',
+    templateUrl: './flower-page.component.html',
+    styleUrls: ['./flower-page.component.scss'],
+    encapsulation: ViewEncapsulation.None
 })
-export class FlowerPageComponent implements OnInit, AfterViewInit  {
-  @Output() menuClosed: EventEmitter<any> = new EventEmitter<any>();
-  @ViewChild('slider', {static: true}) slider: ElementRef;
-  id: string;
+export class FlowerPageComponent implements OnInit, AfterViewInit {
+    @Output() menuClosed: EventEmitter<any> = new EventEmitter<any>();
+    @ViewChild('distortionSlider', {static: true}) distortionSlider: ElementRef;
+    id: string;
 
-  page: Page;
+    page: Page;
 
-  constructor(private route: ActivatedRoute,
-              private flowersService: FlowersService) {
-  }
+    constructor(private route: ActivatedRoute,
+                private flowersService: FlowersService) {
+    }
 
-  ngOnInit() {
-    this.route.params.subscribe((params: Params) => {
-      this.page = this.flowersService.getById(+params.id);
-    });
-  }
+    ngOnInit() {
+        this.route.params.subscribe((params: Params) => {
+            this.page = this.flowersService.getById(+params.id);
+        });
+    }
 
-  ngAfterViewInit() {
+    ngAfterViewInit() {
 
-    // const myAnimation = new DistortionEffect({
-    //   parent: this.distort.nativeElement,
-    //   intensity: 0.3,
-    //   hover: false,
-    //   image1: './assets/images/Img1.jpg',
-    //   image2: './assets/images/Img2.jpg',
-    //   image3: './assets/images/Img3.jpg',
-    //   image4: './assets/images/Img4.jpg',
-    //   displacementImage: './assets/images/distortion.jpg',
-    //   btnNext: '.asd'
-    // });
-
-      const el = this.slider.nativeElement;
-      const imgs = Array.from(el.querySelectorAll('img'));
-      const myAnimation = DistortionEffect({
-        parent: el,
-        images: imgs
-      });
-  }
+        const el = this.distortionSlider.nativeElement;
+        const imgs = Array.from(el.querySelectorAll('img'));
+        const myAnimation = DistortionEffect({
+            parent: el,
+            images: imgs
+        });
+    }
 }
--- src/app/plugins/distortion-effect.ts
+++ src/app/plugins/distortion-effect.ts
@@ -2,7 +2,7 @@ import * as THREE from 'three';
 import {TweenLite, Expo} from 'gsap';
 
 export const DistortionEffect = opts => {
-  const vertex = `
+    const vertex = `
         varying vec2 vUv;
         void main() {
           vUv = uv;
@@ -10,7 +10,7 @@ export const DistortionEffect = opts => {
         }
     `;
 
-  const fragment = `
+    const fragment = `
         varying vec2 vUv;
         uniform sampler2D currentImage;
         uniform sampler2D nextImage;
@@ -29,176 +29,174 @@ export const DistortionEffect = opts => {
         }
     `;
 
-  let images = opts.images, image, sliderImages = [];
-  const canvasWidth = images[0].clientWidth;
-  const canvasHeight = images[0].clientHeight;
-  const parent = opts.parent;
-  const renderWidth = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
-  const renderHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+    const images = opts.images;
+    let image;
+    let sliderImages = [];
+    const canvasWidth = images[0].parentNode.clientWidth;
+    const canvasHeight = images[0].parentNode.clientWidth * 1.085;
+    const parent = opts.parent;
+    // const renderWidth = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+    // const renderHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
 
-  let renderW, renderH;
+    let renderW;
+    let renderH;
 
-  if (renderWidth > canvasWidth) {
-    renderW = renderWidth;
-  } else {
     renderW = canvasWidth;
-  }
+    renderH = canvasHeight;
 
-  renderH = canvasHeight;
-
-  const renderer = new THREE.WebGLRenderer({
-    antialias: false,
-  });
-
-  renderer.setPixelRatio(window.devicePixelRatio);
-  renderer.setClearColor(0x23272A, 1.0);
-  renderer.setSize(renderW, renderH);
-  parent.appendChild(renderer.domElement);
-
-  const loader = new THREE.TextureLoader();
-  loader.crossOrigin = 'anonymous';
-
-  images.forEach((img) => {
-    image = loader.load(img.getAttribute('src') + '?v=' + Date.now());
-    image.magFilter = image.minFilter = THREE.LinearFilter;
-    image.anisotropy = renderer.capabilities.getMaxAnisotropy();
-    sliderImages.push(image);
-  });
-
-  const scene = new THREE.Scene();
-  scene.background = new THREE.Color(0x23272A);
-  const camera = new THREE.OrthographicCamera(
-    renderWidth / -2,
-    renderWidth / 2,
-    renderHeight / 2,
-    renderHeight / -2,
-    1,
-    1000
-  );
-
-  camera.position.z = 1;
-
-  const mat = new THREE.ShaderMaterial({
-    uniforms: {
-      dispFactor: {type: 'f', value: 0.0},
-      currentImage: {type: 't', value: sliderImages[0]},
-      nextImage: {type: 't', value: sliderImages[1]},
-    },
-    vertexShader: vertex,
-    fragmentShader: fragment,
-    transparent: true,
-    opacity: 1.0
-  });
-
-  const geometry = new THREE.PlaneBufferGeometry(
-    parent.offsetWidth,
-    parent.offsetHeight,
-    1
-  );
-  const object = new THREE.Mesh(geometry, mat);
-  object.position.set(0, 0, 0);
-  scene.add(object);
-
-  const addEvents = () => {
-    const pagButtons = Array.from(document.getElementById('pagination').querySelectorAll('button'));
-    let isAnimating = false;
-
-    pagButtons.forEach((el) => {
-
-      el.addEventListener('click', function() {
-
-        if (!isAnimating) {
-
-          isAnimating = true;
-
-          document.getElementById('pagination').querySelectorAll('.active')[0].className = '';
-          this.className = 'active';
-
-          const slideId = parseInt(this.dataset.slide, 10);
-
-          mat.uniforms.nextImage.value = sliderImages[slideId];
-          // mat.uniforms.nextImage.needsUpdate = true;
-
-          TweenLite.to(mat.uniforms.dispFactor, 1, {
-            value: 1,
-            ease: 'Expo.easeInOut',
-            onComplete() {
-              mat.uniforms.currentImage.value = sliderImages[slideId];
-              // mat.uniforms.currentImage.needsUpdate = true;
-              mat.uniforms.dispFactor.value = 0.0;
-              isAnimating = false;
-            }
-          });
-
-          // const slideTitleEl = document.getElementById('slide-title');
-          // const slideStatusEl = document.getElementById('slide-status');
-          // const nextSlideTitle = document.querySelectorAll(`[data-slide-title="${slideId}"]`)[0].innerHTML;
-          // const nextSlideStatus = document.querySelectorAll(`[data-slide-status="${slideId}"]`)[0].innerHTML;
-
-          // TweenLite.fromTo(slideTitleEl, 0.5,
-          //   {
-          //     autoAlpha: 1,
-          //     filter: 'blur(0px)',
-          //     y: 0
-          //   },
-          //   {
-          //     autoAlpha: 0,
-          //     filter: 'blur(10px)',
-          //     y: 20,
-          //     ease: 'Expo.easeIn',
-          //     onComplete() {
-          //       slideTitleEl.innerHTML = nextSlideTitle;
-          //
-          //       TweenLite.to(slideTitleEl, 0.5, {
-          //         autoAlpha: 1,
-          //         filter: 'blur(0px)',
-          //         y: 0,
-          //       });
-          //     }
-          //   });
-          //
-          // TweenLite.fromTo(slideStatusEl, 0.5,
-          //   {
-          //     autoAlpha: 1,
-          //     filter: 'blur(0px)',
-          //     y: 0
-          //   },
-          //   {
-          //     autoAlpha: 0,
-          //     filter: 'blur(10px)',
-          //     y: 20,
-          //     ease: 'Expo.easeIn',
-          //     onComplete() {
-          //       slideStatusEl.innerHTML = nextSlideStatus;
-          //
-          //       TweenLite.to(slideStatusEl, 0.5, {
-          //         autoAlpha: 1,
-          //         filter: 'blur(0px)',
-          //         y: 0,
-          //         delay: 0.1,
-          //       });
-          //     }
-          //   });
-
-        }
-
-      });
-
+    const renderer = new THREE.WebGLRenderer({
+        antialias: true,
     });
 
-  };
-
-  addEvents();
-
-  window.addEventListener('resize', e => {
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setClearColor(0x3B181E, 1.0);
     renderer.setSize(renderW, renderH);
-  });
+    parent.appendChild(renderer.domElement);
 
-  const animate = () => {
-    requestAnimationFrame(animate);
+    const loader = new THREE.TextureLoader();
+    loader.crossOrigin = 'anonymous';
 
-    renderer.render(scene, camera);
-  };
-  animate();
+    images.forEach((img) => {
+        image = loader.load(img.getAttribute('src') + '?v=' + Date.now());
+        image.magFilter = image.minFilter = THREE.LinearFilter;
+        image.anisotropy = renderer.capabilities.getMaxAnisotropy();
+        sliderImages.push(image);
+    });
+
+    const scene = new THREE.Scene();
+    scene.background = new THREE.Color(0x3B181E);
+    const camera = new THREE.OrthographicCamera(
+        canvasWidth / -2.0,
+        canvasWidth / 2.0,
+        canvasHeight / 2.0,
+        canvasHeight / -2.0,
+        1,
+        1000
+    );
+
+    camera.position.z = 1;
+
+    const mat = new THREE.ShaderMaterial({
+        uniforms: {
+            dispFactor: {type: 'f', value: 0.0},
+            currentImage: {type: 't', value: sliderImages[0]},
+            nextImage: {type: 't', value: sliderImages[1]},
+        },
+        vertexShader: vertex,
+        fragmentShader: fragment,
+        transparent: true,
+        opacity: 1.0
+    });
+
+    const geometry = new THREE.PlaneBufferGeometry(
+        parent.offsetWidth,
+        parent.offsetHeight,
+        1
+    );
+    const object = new THREE.Mesh(geometry, mat);
+    object.position.set(0, 0, 0);
+    scene.add(object);
+
+    const addEvents = () => {
+        const pagButtons = Array.from(document.getElementById('pagination').querySelectorAll('button'));
+        let isAnimating = false;
+
+        pagButtons.forEach((el) => {
+
+            el.addEventListener('click', function() {
+
+                if (!isAnimating) {
+
+                    isAnimating = true;
+
+                    document.getElementById('pagination').querySelectorAll('.active')[0].className = '';
+                    this.className = 'active';
+
+                    const slideId = parseInt(this.dataset.slide, 10);
+
+                    mat.uniforms.nextImage.value = sliderImages[slideId];
+                    // mat.uniforms.nextImage.needsUpdate = true;
+
+                    TweenLite.to(mat.uniforms.dispFactor, 1, {
+                        value: 1,
+                        ease: 'Expo.easeInOut',
+                        onComplete() {
+                            mat.uniforms.currentImage.value = sliderImages[slideId];
+                            // mat.uniforms.currentImage.needsUpdate = true;
+                            mat.uniforms.dispFactor.value = 0.0;
+                            isAnimating = false;
+                        }
+                    });
+
+                    // const slideTitleEl = document.getElementById('slide-title');
+                    // const slideStatusEl = document.getElementById('slide-status');
+                    // const nextSlideTitle = document.querySelectorAll(`[data-slide-title="${slideId}"]`)[0].innerHTML;
+                    // const nextSlideStatus = document.querySelectorAll(`[data-slide-status="${slideId}"]`)[0].innerHTML;
+
+                    // TweenLite.fromTo(slideTitleEl, 0.5,
+                    //   {
+                    //     autoAlpha: 1,
+                    //     filter: 'blur(0px)',
+                    //     y: 0
+                    //   },
+                    //   {
+                    //     autoAlpha: 0,
+                    //     filter: 'blur(10px)',
+                    //     y: 20,
+                    //     ease: 'Expo.easeIn',
+                    //     onComplete() {
+                    //       slideTitleEl.innerHTML = nextSlideTitle;
+                    //
+                    //       TweenLite.to(slideTitleEl, 0.5, {
+                    //         autoAlpha: 1,
+                    //         filter: 'blur(0px)',
+                    //         y: 0,
+                    //       });
+                    //     }
+                    //   });
+                    //
+                    // TweenLite.fromTo(slideStatusEl, 0.5,
+                    //   {
+                    //     autoAlpha: 1,
+                    //     filter: 'blur(0px)',
+                    //     y: 0
+                    //   },
+                    //   {
+                    //     autoAlpha: 0,
+                    //     filter: 'blur(10px)',
+                    //     y: 20,
+                    //     ease: 'Expo.easeIn',
+                    //     onComplete() {
+                    //       slideStatusEl.innerHTML = nextSlideStatus;
+                    //
+                    //       TweenLite.to(slideStatusEl, 0.5, {
+                    //         autoAlpha: 1,
+                    //         filter: 'blur(0px)',
+                    //         y: 0,
+                    //         delay: 0.1,
+                    //       });
+                    //     }
+                    //   });
+
+                }
+
+            });
+
+        });
+
+    };
+
+    addEvents();
+
+    window.addEventListener('resize', e => {
+        renderer.setSize(renderW, renderH);
+    });
+
+    const animate = () => {
+        requestAnimationFrame(animate);
+
+        renderer.render(scene, camera);
+    };
+    animate();
 };
 
