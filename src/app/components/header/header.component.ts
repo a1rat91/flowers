bf165e7b94b4be22b986e1d7e971930141978ff5
Airat Khairullin <akhairullin@swiftcom.corp>
2019-10-09 17:16:57 +0300

    master

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,10 +1,11 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, ViewEncapsulation} from '@angular/core';
 import {NavigationService} from '../../services/navigation.service';
 
 @Component({
     selector: 'app-header',
     templateUrl: './header.component.html',
     styleUrls: ['./header.component.scss', './header-burger.component.scss'],
+    encapsulation: ViewEncapsulation.None
 })
 export class HeaderComponent implements OnInit {
 
