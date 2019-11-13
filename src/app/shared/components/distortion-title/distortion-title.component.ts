c322d22731eaadba4307a8a874fa9aeddcfb17f3
Airat Khairullin <akhairullin@swiftcom.corp>
2019-11-13 17:49:31 +0300

    Попытка прикрутить blotter

--- src/app/shared/components/distortion-title/distortion-title.component.ts
+++ src/app/shared/components/distortion-title/distortion-title.component.ts
@@ -1,4 +1,12 @@
-import {Component, DoCheck, ElementRef, HostListener, Inject, OnInit, ViewChild} from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  HostListener,
+  Inject,
+  Input,
+  ViewChild
+} from '@angular/core';
 import {DOCUMENT} from '@angular/common';
 import {MathUtils as MathUtils} from './utils';
 
@@ -9,31 +17,27 @@ declare var Blotter: any;
   templateUrl: './distortion-title.component.html',
   styleUrls: ['./distortion-title.component.scss']
 })
-export class DistortionTitleComponent implements OnInit, DoCheck {
+export class DistortionTitleComponent implements AfterViewInit {
 
+  @ViewChild('elem', {static: true}) elem: ElementRef;
+  @ViewChild('textEl', {static: true}) textEl: ElementRef;
+  @Input() titleText;
   body;
   docEl;
   winsize;
   mousePos;
-  @ViewChild('elem', {static: true}) elem: ElementRef;
-  @ViewChild('textEl', {static: true}) textEl: ElementRef;
-   // View child
 
   constructor(@Inject(DOCUMENT) private document: Document) {
     this.body = document.body;
     this.docEl = document.documentElement;
   }
 
-  ngOnInit() {
+  ngAfterViewInit() {
     this.mousePos = {x: window.innerWidth / 2, y: window.innerHeight / 2};
     this.calcWinsize();
 
     window.addEventListener('mousemove', ev => this.mousePos = this.getMousePos(ev));
     this.createBlotterText();
-  }
-
-  ngDoCheck() {
-
   }
 
   @HostListener('window:resize', ['$event'])
@@ -43,7 +47,6 @@ export class DistortionTitleComponent implements OnInit, DoCheck {
 
   calcWinsize = () => {
     this.winsize = {width: window.innerWidth, height: window.innerHeight};
-    console.log(this.winsize, 'winsize');
     return this.winsize;
   }
 
@@ -68,19 +71,19 @@ export class DistortionTitleComponent implements OnInit, DoCheck {
       family : "'Playfair Display',serif",
       weight: 700,
       size : 120,
-      paddingLeft: 100,
-      paddingRight: 100,
-      paddingTop: 100,
-      paddingBottom: 100,
+      paddingLeft: 0,
+      paddingRight: 80,
+      paddingTop: 0,
+      paddingBottom: 0,
       fill : 'white'
     });
 
     this.elem.nativeElement.removeChild(this.textEl.nativeElement);
 
     const material = new Blotter.LiquidDistortMaterial();
-    material.uniforms.uSpeed.value = 0.5;
+    material.uniforms.uSpeed.value = 1;
     material.uniforms.uVolatility.value = 0;
-    material.uniforms.uSeed.value = 0.1;
+    material.uniforms.uSeed.value = 0.5;
     const blotter = new Blotter(material, {texts: text});
     const scope = blotter.forText(text);
     scope.appendTo(this.elem.nativeElement);
@@ -93,7 +96,7 @@ export class DistortionTitleComponent implements OnInit, DoCheck {
       const relmousepos = {x : this.mousePos.x - docScrolls.left, y : this.mousePos.y - docScrolls.top };
       const mouseDistance = MathUtils.distance(lastMousePosition.x, relmousepos.x, lastMousePosition.y, relmousepos.y);
 
-      volatility = MathUtils.lerp(volatility, Math.min(MathUtils.lineEq(0.1, 0, 10, 0, mouseDistance), 0.1), 0.005);
+      volatility = MathUtils.lerp(volatility, Math.min(MathUtils.lineEq(0.045, 0, 0.045, 0, mouseDistance), 0.045), 0.002);
       material.uniforms.uVolatility.value = volatility;
 
       lastMousePosition = {x: relmousepos.x, y: relmousepos.y};
